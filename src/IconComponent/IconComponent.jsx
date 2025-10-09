4c83aae9e19045015a39859353dce51b0df66d51
Nathan <nverjinski@rapidsos.com>
2025-10-08 21:38:38 -0500

    fix: made icon render from bottom up

--- src/IconComponent/IconComponent.jsx
+++ src/IconComponent/IconComponent.jsx
@@ -5,6 +5,9 @@ const MarkerContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
 `;
 
 const Pill = styled.div`

702724de74f29a55c286fc260d14b5302d87d761
Nathan <nverjinski@rapidsos.com>
2025-10-08 20:27:30 -0500

    feat: added a scaled up iconComponent

--- src/IconComponent/IconComponent.jsx
+++ src/IconComponent/IconComponent.jsx
@@ -1,14 +1,5 @@
 import styled from "styled-components";
-import { BuildingLibraryIcon } from "@heroicons/react/24/solid";
-
-// This wrapper creates the 512x512 canvas for the image export.
-const DownloadWrapper = styled.div`
-  width: 512px;
-  height: 512px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
 
 const MarkerContainer = styled.div`
   display: flex;
@@ -18,10 +9,9 @@ const MarkerContainer = styled.div`
 
 const Pill = styled.div`
   padding: 30px; /* 3px * 10 */
-  background-color: #000000;
+  background-color: #dc0206;
   border-radius: 50%;
-  color: #ffffff;
-  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.3); /* (2px, 5px) * 10 */
+  color: #000000;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -33,8 +23,7 @@ const Pill = styled.div`
 const PinLine = styled.div`
   width: 20px; /* 2px * 10 */
   height: 200px; /* 20px * 10 */
-  background-color: #000000;
-  box-shadow: 0 20px 50px rgba(0, 0, 0, 0.3); /* (2px, 5px) * 10 */
+  background-color: #dc0206;
 `;
 
 const IconWrapper = styled.div`
@@ -45,16 +34,14 @@ const IconWrapper = styled.div`
 
 const IconComponent = () => {
   return (
-    <DownloadWrapper>
-      <MarkerContainer>
-        <Pill>
-          <IconWrapper>
-            <BuildingLibraryIcon />
-          </IconWrapper>
-        </Pill>
-        <PinLine />
-      </MarkerContainer>
-    </DownloadWrapper>
+    <MarkerContainer>
+      <Pill>
+        <IconWrapper>
+          <ExclamationTriangleIcon />
+        </IconWrapper>
+      </Pill>
+      <PinLine />
+    </MarkerContainer>
   );
 };
 
